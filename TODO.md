37b095a38018cc311d4ee35f5d2f5b6688765581
Xie Yuheng <xyheme@gmail.com>
2024-08-24 19:41:51 +0800

    [cps] `ramify` -- 用 `problemPathHasLoop` 过滤掉带有 loop 的 path

--- TODO.md
+++ TODO.md
@@ -1,14 +1,14 @@
-[cps] `breadthFirstExplore` -- 过滤掉带有 loop 的 path
+[cps] [refactoring] `ProblemPath` 保存它所属于的 space
+
+- 这样是合理的，因为一个 Path 之后在某个 space 中才有意义，
+  并且能大大简化 API -- `ramify` 只需要一个参数。
 
 [cps] 实现第三章的地铁例子
+
 [cps] 实现第三章的代数例子
 
-[cps] 用一个带有 `Searching` 内容的 `Solver` class 来实现
+[cps] [refactoring] 用单个的 `Solver` class 来实现 -- 而不是 `ProblemSpace` + `Searching`
 
-- 有 `ProblemSpace` + `Searching` 这两个 class 看来太复杂了,
+- 因为 `ProblemSpace` + `Searching` 这两个 class 看来太复杂了,
   比如 `searching.space.problemIsSolved(path.current)`
   v.s. `solver.problemIsSolved(path.current)`
-
-- 也许可以让 `ProblemPath` 保存它所属于的 space，
-  这样是合理的，因为一个 Path 之后在某个 space 中才有意义，
-  并且能大大简化 API -- `ramify` 只需要一个参数。
--- src/cps/ProblemPath.ts
+++ src/cps/ProblemPath.ts
@@ -1,5 +1,11 @@
 import { type ProblemSpace } from "./ProblemSpace.js"
 
+// 假设我们用 (p) 来表示 Problem，用 [b] 来表示 Branch，
+// 那么形如 (p0) -> [b0] -> (p1) -> [b1] -> (p2)
+// 的 Path，会被实现为：
+// - prefix: [[p0, b0], [p1, b1]]
+// - current: p2
+
 export type ProblemPath<Problem, Branch> = {
   prefix: Array<[Problem, Branch]>
   current: Problem
@@ -25,12 +31,26 @@ export function extendProblemPath<Problem, Branch>(
   }
 }
 
+export function problemPathHasLoop<Branch, Problem>(
+  space: ProblemSpace<Branch, Problem>,
+  path: ProblemPath<Branch, Problem>,
+): boolean {
+  for (const [problem, branch] of path.prefix) {
+    if (space.problemEqual(problem, path.current)) {
+      return true
+    }
+  }
+
+  return false
+}
+
 export function ramify<Branch, Problem>(
   space: ProblemSpace<Problem, Branch>,
   path: ProblemPath<Problem, Branch>,
 ): Array<ProblemPath<Problem, Branch>> {
   const branches = space.validBranches(path.current)
-  return branches.map((branch) =>
+  const newPaths = branches.map((branch) =>
     extendProblemPath(path, branch, space.branchApply(branch, path.current)),
   )
+  return newPaths.filter((newPath) => problemPathHasLoop(space, newPath))
 }
